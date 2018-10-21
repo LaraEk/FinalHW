76122e1bca926dddd252a06b1e837a39b3f12921
Ekscursionist <larakek@gmail.com>
2018-10-20 20:48:35 -0400

    connecting the server, which now connects!

--- nytreact/server.js
+++ nytreact/server.js
@@ -3,31 +3,45 @@ const bodyParser = require ("body-parser");
 const mongoose = require("mongoose");
 const cheerio = require("cheerio");
 const request = require("request");
+// const axios = require("axios"); (I'll only include this if I end up using axios)
+
+const routes = require("./Routes");
+// const db = require("./ModelsandControllers/models");
 
 // const Article = require ("./ModelController/models/Article");
 
-// const app = express();
-// const PORT = process.env.PORT || 5656;
+const app = express();
+const PORT = process.env.PORT || 5656;
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended:true}));
-// a
-// FILL THIS IN LATER first get hte scraper scraping
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("client"));
+//app.use(routes);
 
-console.log(" ----- scraper scraping ----- ");
-request("https://old.reddit.com/r/relationships/", function(err, resp, html) {
-    var $ = cheerio.load(html);
-    var results = [];
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/etymapsdb");
 
-    $("p.title").each(function(i, element) {
-        var title = $(element).text();
-        var link = $(element).children().attr("href");
 
-        results.push({
-            title: title,
-            link: link
-        });
-    });
-
-console.log(results);
+app.listen(PORT, function() {
+    console.log("connected on " + PORT);
 });
+
+
+// THIS WORKS
+// console.log(" ----- scraper scraping ----- ");
+// request("https://old.reddit.com/r/etymologymaps/", function(err, resp, html) {
+//     var $ = cheerio.load(html);
+//     var results = [];
+
+//     $("p.title").each(function(i, element) {
+//         var title = $(element).text();
+//         var link = $(element).children().attr("href");
+
+//         results.push({
+//             title: title,
+//             link: link
+//         });
+//     });
+
+// console.log(results);
+// });
